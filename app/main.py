4f4a2998671ebe5e0e5132fcdf1ae3d7d434d334
Your Name <you@example.com>
2025-10-14 18:13:50 +0700

    CHECLIST ITEM #4

--- app/main.py
+++ app/main.py
@@ -9,16 +9,14 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
+    Person.people.clear()
     people_list = [Person(person["name"], person["age"]) for person in people]
-
     for person in people:
-        spouse = "husband" if "husband" in person.keys() else "wife"
         name = person["name"]
-        spouse_name = person[spouse]
-        if spouse_name:
-            if spouse == "husband":
-                Person.people[name].husband = Person.people[spouse_name]
-            else:
-                Person.people[name].wife = Person.people[spouse_name]
+        spouse_name = person.get("husband") or person.get("wife")
+        if person.get("husband"):
+            Person.people[name].husband = Person.people[spouse_name]
+        elif person.get("wife"):
+            Person.people[name].wife = Person.people[spouse_name]
 
     return people_list
